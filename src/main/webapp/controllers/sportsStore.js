8d3f5ac801aff15ae52f16a8d4ac2377a5401c8f
zOSMFAdmin <zOSMFAdmin@10.0.2.15>
2015-09-10 21:57:19 +0800

    a demo AngularJS app--SportsStore

---
+++ src/main/webapp/controllers/sportsStore.js
@@ -0,0 +1,50 @@
+angular.module("sportsStore").constant("dataUrl",
+		"http://9.115.116.103:5500/products").constant("orderUrl",
+		"http://9.15.116.103:5500/orders").controller("sportsStoreCtrl",
+		function($scope, $http, dataUrl, orderUrl, cart) {
+			// $scope.data = {
+			// products : [ {
+			// name : "Product #1",
+			// description : "A product",
+			// category : "Category #1",
+			// price : 100
+			// }, {
+			// name : "Product #2",
+			// description : "A product",
+			// category : "Category #1",
+			// price : 110
+			// }, {
+			// name : "Product #3",
+			// description : "A product",
+			// category : "Category #2",
+			// price : 210
+			// }, {
+			// name : "Product #4",
+			// description : "A product",
+			// category : "Category #3",
+			// price : 202
+			// } ]
+			// };
+
+			$scope.data = {};
+
+			$http.get(dataUrl).success(function(data) {
+				$scope.data.products = data;
+			}).error(function(error) {
+				$scope.data.error = error;
+			});
+
+			$scope.sendOrder = function(shippingDetails) {
+				console.log("sendOrder method called!");
+				var order = angular.copy(shippingDetails);
+				$http.post(orderUrl, order).success(function(data) {
+					$scope.data.orderId = data.id;
+				}).error(function(error){
+					$scope.data.orderError=error;
+				}).then(function(){
+					console.log("go to complete page!!!");
+					$location.path("/complete");
+				});
+				cart.getProducts().length = 0;
+			}
+		});
