76dfee6dca51dacdbc10b84a47b0706f91c99c6a
Xi Xue Jia <xixuejia@gmail.com>
2015-09-12 22:32:34 +0800

    Removed the swipe navigation support, so the jquery bcSwipe are no longer required

--- src/main/webapp/js/agenda.js
+++ src/main/webapp/js/agenda.js
@@ -1,26 +1,25 @@
 $(document).ready(function() {
-	$('.carousel').bcSwipe({
-		threshold : 50
-	});
 
 	var tabTemplate = '<li role="presentation" class="${active}"><a href="#${id}" aria-controls="${date}" role="tab" data-toggle="tab">${date}</a></li>';
 	$.get('views/agendaTemplate.html', function(template) {
-
 		$.get('data/agenda.json?' + new Date().getTime(), function(agendas) {
 			$.each(agendas, function(index, agenda) {
 				agenda.id = "agenda" + index;
                 if (index === 0) {
-                    agenda.itemClass = "item active";
                     agenda.active = "active";
-                } else {
-                    agenda.itemClass = "item";
                 }
 //				console.log("agenda:"+JSON.stringify(agenda));
-				agenda.id = "agenda" + index;
 				var page = $.tmpl(template, agenda);
-				page.appendTo('.carousel-inner');
+				page.appendTo('#agenda');
 				$('#tabs').append($.tmpl(tabTemplate, agenda));
+			});
+			
+			console.log("binding...");
+			$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+				//The window will scroll to top after user activate a new tab
+				$(window).scrollTop(0);
 			});
 		});
 	});
+	
 });
